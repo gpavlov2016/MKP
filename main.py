2a1e19ce9009dc9fc4b1b4316f6c3e7ed59446e6
Guy Pavlov <guy.pavlov84@gmail.com>
2016-09-23 03:05:03 -0700

    est calculated

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-
+import sys
 #jobs = []
-jobs =  {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
-
+jobs =  {}
 resources = []
 
 with open('jobs.txt', 'r') as f:
     for line in f:
         words = line.strip().split(":")
         if words[1] == '':
-            jobs.append([words[0], 0, 0, []])
-#            jobs[words[0]] = ""
+            job = words[0]
+            jobs[job] = {'children': set(), 'est': 0, 'lst': 0}
         elif words[0] == 'cores_required':
-            jobs[-1][1] = words[1]
+            jobs[job]['cores'] = int(words[1])
         elif words[0] == 'execution_time':
-            jobs[-1][2] = words[1]
+            jobs[job]['time'] = int(words[1])
         elif words[0] == 'parent_tasks':
             tasks = words[1].replace('"', '').strip().split(",")
             tasks = [x.strip() for x in tasks]
-            jobs[-1][3] = tasks
+            jobs[job]['parents'] = set(tasks)
+            for task in tasks:
+                jobs[task]['children'].add(job)
         else:
             print("Jobs file parsing error: " + ', '.join(words))
             exit()
@@ -31,20 +27,12 @@ f.closed
 with open('resources.txt', 'r') as f:
     for line in f:
         words = line.strip().split(":")
-        resources.append(words)
+        resources.append([words[0], int(words[1])])
 f.closed
 
 print jobs
 print resources
 
-est = []
-
-graph = {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
 
 def dfs(graph, start):
     visited, stack = set(), [start]
@@ -52,7 +40,33 @@ def dfs(graph, start):
         vertex = stack.pop()
         if vertex not in visited:
             visited.add(vertex)
-            stack.extend(graph[vertex] - visited)
+            if 'children' in graph[vertex]:
+                stack.extend(graph[vertex]['children'] - visited)
     return visited
 
-print dfs(graph, 'A') # {'E', 'D', 'F', 'A', 'C', 'B'}
+visited = set()
+for job in jobs:
+    if job in visited:
+        continue
+    stack = [job]
+    while stack:
+        vertex = stack.pop()
+        if 'parents' in jobs[vertex]:
+            prnts_notvisited = [x for x in jobs[vertex]['parents'] if x not in visited]
+            prnts_visited = [x for x in jobs[vertex]['parents'] if x in visited]
+            if len(prnts_notvisited) == 0:
+                jobs[vertex]['est'] = max([jobs[x]['est'] + jobs[x]['time'] for x in prnts_visited])
+                visited.add(vertex)
+            else:
+                stack.append(vertex)
+                stack += prnts_notvisited
+        else:
+            jobs[vertex]['est'] = 0
+            visited.add(vertex)
+print 'jobs:'
+print jobs
+
+total_cores = sum([x[1] for x in resources])
+print 'total_cores: ' + str(total_cores)
+
+total
